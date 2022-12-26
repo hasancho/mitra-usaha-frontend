74643fc9d64ae469c1dd631b3fc9d626ddaabdb1
hasancho <hasankhoiry@gmail.com>
2022-12-26 17:26:19 +0700

    modify nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ import colors from "vuetify/es5/util/colors";
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  target: "static",
   head: {
     titleTemplate: "%s - frontend",
     title: "frontend",
@@ -30,6 +31,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/vuetify
     "@nuxtjs/vuetify",
+    "@nuxtjs/dotenv",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -42,7 +44,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "http://localhost:3001",
+    baseURL: process.env.BASE_URL,
   },
 
   auth: {
